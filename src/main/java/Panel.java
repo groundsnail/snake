a7878bf05f20bf8898a8204c1579ff1898782096
groundsnail <52134052+groundsnail@users.noreply.github.com>
2022-04-03 09:58:53 +0800

    keyboard accept

--- src/main/java/Panel.java
+++ src/main/java/Panel.java
@@ -1,13 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Panel extends JPanel {
+public class Panel extends JPanel implements KeyListener {
     int length;
     int[] x =new int[100];
     int[] y =new int[100];
-
+    boolean isStart = false;
+//    Timer tme =new Timer(100,this);
     public Panel() {
         init();
+        this.setFocusable(true); //获取焦点事件
+        this.addKeyListener(this); //键盘监听事件
+//        timer.start();
     }
 
     public void init(){
@@ -27,5 +35,32 @@ public class Panel extends JPanel {
         for (int i = 1; i < length; i++) {
             Data.ibody.paintIcon(this,g,x[i],y[i]);
         }
+        if (isStart == true) {
+            g.drawString("开始游戏",200,200);
+
+        }
     }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode=e.getKeyCode();
+        if (keyCode == KeyEvent.VK_S) {
+            isStart=!isStart;
+
+            repaint();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+
+
 }
