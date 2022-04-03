a7b65004820ba94f3fff0e343ef592816dcb056d
groundsnail <52134052+groundsnail@users.noreply.github.com>
2022-04-03 19:43:09 +0800

    movement added

--- src/main/java/Data.java
+++ src/main/java/Data.java
@@ -6,6 +6,13 @@ public class Data {
    public static ImageIcon app = new ImageIcon(headURL);
    public static URL body =Data.class.getResource("body.png" );
    public static ImageIcon ibody =new ImageIcon(body);
-
+   public static URL lT = Data.class.getResource("left.png");
+   public static ImageIcon appL = new ImageIcon(lT);
+   public static URL dN = Data.class.getResource("down.png");
+   public static ImageIcon appD = new ImageIcon(dN);
+   public static URL uP = Data.class.getResource("up.png");
+   public static ImageIcon appU = new ImageIcon(uP);
+   public static URL fD = Data.class.getResource("food.png");
+   public static ImageIcon food = new ImageIcon(fD);
 
 }
--- src/main/java/Panel.java
+++ src/main/java/Panel.java
@@ -10,7 +10,10 @@ public class Panel extends JPanel implements KeyListener, ActionListener {
     int[] x =new int[100];
     int[] y =new int[100];
     boolean isStart = false;
+    String direction = "R";
     Timer tme =new Timer(100,this);
+    int foodX=(int)(Math.random()*1000);
+    int foodY=(int)(Math.random()*1000);
     public Panel() {
         init();
         this.setFocusable(true); //获取焦点事件
@@ -31,14 +34,27 @@ public class Panel extends JPanel implements KeyListener, ActionListener {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         this.setBackground(Color.BLACK);
-        Data.app.paintIcon(this,g,x[0],y[0]);
+        if (direction.equals("R")) {
+            Data.app.paintIcon(this,g,x[0],y[0]);
+        }
+        if (direction.equals("L")) {
+            Data.appL.paintIcon(this,g,x[0],y[0]);
+        }
+        if (direction.equals("D")) {
+            Data.appD.paintIcon(this,g,x[0],y[0]);
+        }
+        if (direction.equals("U")) {
+            Data.appU.paintIcon(this,g,x[0],y[0]);
+        }
+
         for (int i = 1; i < length; i++) {
             Data.ibody.paintIcon(this,g,x[i],y[i]);
         }
-        if (isStart == true) {
+        if (isStart == false) {
             g.drawString("开始游戏",200,200);
 
         }
+        else {Data.food.paintIcon(this,g,foodX,foodY);}
     }
 
     @Override
@@ -54,6 +70,18 @@ public class Panel extends JPanel implements KeyListener, ActionListener {
 
             repaint();
         }
+        if (keyCode == KeyEvent.VK_RIGHT) {
+            direction= "R";
+        }
+        if (keyCode == KeyEvent.VK_LEFT) {
+            direction= "L";
+        }
+        if (keyCode == KeyEvent.VK_DOWN) {
+            direction= "D";
+        }
+        if (keyCode == KeyEvent.VK_UP) {
+            direction= "U";
+        }
     }
 
     @Override
@@ -65,14 +93,43 @@ public class Panel extends JPanel implements KeyListener, ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (isStart == true) {
-
-            for (int i = length-1; i >0 ; i--) {
-                x[i]=x[i-1];
-                y[i]=y[i-1];
+            if (direction.equals("R")) {
+                for (int i = length-1; i >0 ; i--) {
+                    x[i]=x[i-1];
+                    y[i]=y[i-1];
+                }
+                x[0]=x[0]+30;
+                y[0]=y[0];
+                repaint();
             }
-            x[0]=x[0]+30;
-            y[0]=y[0];
-            repaint();
+            if (direction.equals("L")) {
+                for (int i = length-1; i >0 ; i--) {
+                    x[i]=x[i-1];
+                    y[i]=y[i-1];
+                }
+                x[0]=x[0]-30;
+                y[0]=y[0];
+                repaint();
+            }
+            if (direction.equals("U")) {
+                for (int i = length-1; i >0 ; i--) {
+                    x[i]=x[i-1];
+                    y[i]=y[i-1];
+                }
+                x[0]=x[0];
+                y[0]=y[0]-30;
+                repaint();
+            }
+            if (direction.equals("D")) {
+                for (int i = length-1; i >0 ; i--) {
+                    x[i]=x[i-1];
+                    y[i]=y[i-1];
+                }
+                x[0]=x[0];
+                y[0]=y[0]+30;
+                repaint();
+            }
+
         }
         tme.start();
     }
